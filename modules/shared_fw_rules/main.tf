c2cf2ce7bef3e5e51bb458539d4bc77efb604fcc
Morgante Pell <morgantep@google.com>
2019-04-11 09:11:11 -0700

    allow http

--- modules/shared_fw_rules/main.tf
+++ modules/shared_fw_rules/main.tf
@@ -11,3 +11,17 @@ resource "google_compute_firewall" "allow-https" {
   target_tags   = ["allow-https"]
   source_ranges = ["0.0.0.0/0"]
 }
+
+resource "google_compute_firewall" "allow-http" {
+  name    = "${var.network}-allow-http"
+  network = "${var.network}"
+  project = "${var.host_project}"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["443"]
+  }
+
+  target_tags   = ["allow-http"]
+  source_ranges = ["0.0.0.0/0"]
+}
