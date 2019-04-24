0457f85a45559ae23601d2f02f4e7b0ce17f3553
Morgante Pell <morgantep@google.com>
2019-04-24 13:49:56 -0400

    Add a new instance

--- deployments/app1/dev/main.tf
+++ deployments/app1/dev/main.tf
@@ -16,3 +16,23 @@ resource "google_compute_instance" "backend" {
     subnetwork = "${data.terraform_remote_state.networking-dev.app_subnet}"
   }
 }
+
+resource "google_compute_instance" "backend" {
+  name         = "my-frontend-instance"
+  machine_type = "n1-standard-1"
+  zone         = "us-west1-a"
+  project      = "${var.app_project}"
+
+  tags = ["backend"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    subnetwork = "${data.terraform_remote_state.networking-dev.app_subnet}"
+    access_config {}
+  }
+}
