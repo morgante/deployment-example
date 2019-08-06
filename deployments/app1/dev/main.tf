3a8f67bd349ef1e6ba4826ce029949c95d532f47
Adrien Walkowiak <awalko@google.com>
2019-08-06 10:40:54 -0500

    testing updates

--- deployments/app1/dev/main.tf
+++ deployments/app1/dev/main.tf
@@ -1,4 +1,4 @@
 resource "google_storage_bucket" "image-store-1" {
-  name     = "cf-random-test-image-store-bucket-1"
+  name     = "cf-random-test-image-store-bucket-1-test"
   location = "asia-southeast1"
 }
