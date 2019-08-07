e8f7356fb270f1c6e95bc84e20ecb9961302e93c
Adrien Walkowiak <awalko@google.com>
2019-08-07 08:20:55 -0500

    Fixing backend

--- deployments/shared_data.tf
+++ deployments/shared_data.tf
@@ -1,8 +1,3 @@
-data "terraform_remote_state" "networking-dev" {
+  data "terraform_remote_state" "networking-dev" {
   backend = "gcs"
-
-  config {
-    bucket  = "cf-tf-seed-bucket"
-    prefix  = "tf-demo/state/networking"
-  }
 }
